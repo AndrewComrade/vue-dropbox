26dfbf49075ef88e7ef9c66dc3833f96cda48d02
Andrew Evdokimov <redcomradefl@gmail.com>
2024-03-20 15:51:59 +0300

    SiteLogo, BlueButton components

---
+++ src/components/BlueButton/BlueButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button
+        :type="props.type"
+        class="flex items-center justify-center bg-blue text-white rounded-7"
+    >
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+// TODO: Уточнить про .prop, .attr. Уточнить прокидывание и типизация html атрибутов
+interface Props {
+    type?: 'button' | 'reset' | 'submit'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    type: 'button',
+})
+</script>
+
+<style scoped></style>
